f23ef0fe40f6fc730b8f3ecc60d653db6390f1ac
Júlia Aparecida <julia.aparecida25@gmail.com>
2021-09-11 19:30:36 -0300

    Atualização do jogo Super Trunfo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const naruto = {
     nome:"Naruto",
-    imagem:"img/naruto.png",
+    imagem:"../img/naruto.png",
     atributos:{
         ataque: 7,
         defesas: 6,
@@ -10,7 +10,7 @@ const naruto = {
 
 const batman ={
     nome:"Batman",
-    imagem:"img/batman.jpg",
+    imagem:"../img/batman.jpg",
     atributos:{
         ataque: 6,
         defesas: 8,
@@ -18,11 +18,31 @@ const batman ={
     }
 };
 
-const cartas = [ naruto , batman]; 
+const caitlyn ={
+    nome: "Caitlyn",
+    imagem: "../img/caitlyn.jpg",
+    atributos:{
+        ataque: 7,
+        defesa: 5,
+        magia: 1
+    }
+};
+
+const harry = {
+    nome: "Harry Potter",
+    imagem: "../img/harry.jpg",
+    atributos:{
+        ataque: 7,
+        defesa: 5,
+        magia: 10
+    }
+};
+
+const cartas = [naruto , batman, caitlyn, harry]; 
 let cartaJogador, cartaMaquina;
 
 function SortearCarta(){
-    const numeroDeCartas = 2;
+    const numeroDeCartas = 4;
     let numeroCartaJogador = parseInt(Math.random()*numeroDeCartas);
     let numeroCartaMaquina = parseInt(Math.random()*numeroDeCartas);
     while(numeroCartaJogador == numeroCartaMaquina){
@@ -31,7 +51,6 @@ function SortearCarta(){
     cartaJogador = cartas[numeroCartaJogador];
     cartaMaquina = cartas[numeroCartaMaquina];
     
-    ExibirCartaJogador();
 }
 
 function ExibirCartaJogador(){
@@ -40,19 +59,37 @@ function ExibirCartaJogador(){
     // divCartaJogador.style.backgroundImage = 'url(' + cartaJogador.imagem + ')';
     divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
     document.querySelector(".nome-personagem-jogador").innerText = cartaJogador.nome;
-    // let moldura = '<img src="img/super-trunfo-transparente.png">';
-    // let tagHTML = '<div id="opcoes" class="carta-status">';
-    // let opcoesTexto = '';
-    // for(let atributo in cartaJogador.atributos){
-    //     opcoesTexto += '<input type="radio" class="atributo">';
-    //     opcoesTexto += cartaJogador.atributos[atributo];
-    //     opcoesTexto += "<br>";
-    // }
-    // let nome = cartaJogador.nome;
-    // divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+    let listaDeAtributosDaCarta = document.querySelector(".atributos-jogador");
+    listaDeAtributosDaCarta.innerHTML = PegarAtributos(cartaJogador.atributos);
+}
+
+function ExibirCartaMaquina(){
+    let divCartaMaquina = document.querySelector("#carta-maquina");
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+    document.querySelector(".nome-personagem-maquina").innerText = cartaMaquina.nome;
+    let listaDeAtributosDaCarta = document.querySelector(".atributos-maquina");
+    listaDeAtributosDaCarta.innerHTML = PegarAtributos(cartaMaquina.atributos);
+}
+
+function PegarAtibutosMaquina(){
+
+}
+
+function PegarAtributos(atributosDaCarta){
+    let listaDeAtributos = "", botaoRadio = "";
+    for(let atributo in atributosDaCarta){
+        botaoRadio = `<input type="radio" name="atributo" value="${atributo}">`; 
+        listaDeAtributos += `<li>${atributo} : ${atributosDaCarta[atributo]} ${botaoRadio}</li>`;
+    }
+
+    return listaDeAtributos;
 }
 
 const btnSortear = document.querySelector("#btnSortear");
 btnSortear.onclick = () =>{
     SortearCarta();
+    ExibirCartaJogador();
+    ExibirCartaMaquina();
+
+    document.querySelector("#btnJogar").disabled = false;
 }
